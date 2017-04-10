9586c7b4c9d985250592e20b05f69b0becdbbc34
Jon Franchi <jon.franchi@gmail.com>
2017-04-10 11:06:16 -0500

    Created driver routes + some syntax bug fixes

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var drivers = require('./routes/drivers');
 
 var mongoose = require('mongoose');
 mongoose.connect(process.env.EASYRIDE_DB);
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/drivers', drivers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
